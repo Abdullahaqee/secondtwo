0ca7808139cb79e395fc043efee728bb7a6bbe97
Abdullahaqee <160708061+Abdullahaqee@users.noreply.github.com>
2024-03-14 14:26:24 +0500

    Update Seachdelegate.dart

--- lib/Helper/Seachdelegate.dart
+++ lib/Helper/Seachdelegate.dart
@@ -47,6 +47,8 @@ class MySearchDelegate extends SearchDelegate<String> {
         return ListTile(
           leading: Text(searchResults[index]['cid'].toString()),
           title: Text(searchResults[index]['cname'].toString()),
+          subtitle: Text(searchResults[index]['address1']),
+
           onTap: () {
             // Close the search delegate
             close(context, searchResults[index]['cid'].toString());
